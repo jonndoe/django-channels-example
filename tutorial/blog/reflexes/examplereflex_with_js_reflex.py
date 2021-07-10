1ae15f8875972807e70ba428feb2073827912030
jonndoe <lukichevvasily@gmail.com>
2021-07-10 12:10:59 +0100

    Refactored

--- tutorial/blog/reflexes/examplereflex_with_js_reflex.py
+++ tutorial/blog/reflexes/examplereflex_with_js_reflex.py
@@ -1,6 +1,13 @@
 from sockpuppet.reflex import Reflex
 
+import logging
+
+logger = logging.getLogger('dj-ch-ex-logger')
+
 
 class ExamplereflexWithJsReflex(Reflex):
+    logger.debug('Entered ExamplereflexWithJsReflex(Reflex)')
     def increment(self, step=1):
+        logger.debug('Entered increment method.')
         self.count = int(self.element.dataset['count']) + step
+        logger.debug('Increment method execution finished.')
